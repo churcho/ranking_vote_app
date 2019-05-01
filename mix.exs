ce2c5d1c66e20b0cbedd614ab8236639fd1152a0
slama <s417.lama@gmail.com>
2019-05-01 23:22:36 +0900

    update

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule RankingVoteApp.Mixfile do
     [
       app: :ranking_vote_app,
       version: "0.0.1",
-      elixir: "~> 1.4",
+      elixir: "~> 1.8",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
@@ -32,16 +32,16 @@ defmodule RankingVoteApp.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.2"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_html, "~> 2.10"},
+      {:phoenix            , "~> 1.4.4"          },
+      {:phoenix_pubsub     , "~> 1.0"            },
+      {:phoenix_html       , "~> 2.10"           },
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:distillery, "~> 1.5"},
-      {:diplomat, "~> 0.10.0"},
-      {:croma, "~> 0.9.3"},
-      {:cors_plug, "~> 1.5"},
+      {:gettext            , "~> 0.11"           },
+      {:plug_cowboy        , "~> 2.0"            },
+      {:distillery         , "~> 2.0"            },
+      {:diplomat           , "~> 0.2"            },
+      {:croma              , "~> 0.9.3"          },
+      {:cors_plug          , "~> 1.5"            },
     ]
   end
 end
